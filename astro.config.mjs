9abbee7ab65315ac027270bb71a8711129e08ae0
ZaalRD7109 <ricardozaal@yahoo.com>
2025-09-06 21:37:04 +0200

    astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,2 +1,5 @@
-import { defineConfig } from 'astro/config';
-export default defineConfig({ site: 'https://fountaingrace.org' });
+import { defineConfig } from "astro/config";
+
+export default defineConfig({
+  site: "https://fountaingrace.org", // update if needed
+});
